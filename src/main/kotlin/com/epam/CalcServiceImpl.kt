3dd1caf1d9bc5704e420403584626dee22740b11
Kirill Peshin <kirill_peshin@epam.com>
2021-02-16 13:11:51 +0300

    Upd

--- src/main/kotlin/com/epam/CalcServiceImpl.kt
+++ src/main/kotlin/com/epam/CalcServiceImpl.kt
@@ -2,19 +2,19 @@ package com.epam
 
 class CalcServiceImpl : CalcService {
     override fun add(a: Int, b: Int) : Int {
-       return 0
+        return a + b
     }
 
     override fun multiply(a: Int, b: Int): Int {
-        return 0
+        return a * b
     }
 
     override fun subtract(a: Int, b: Int): Int {
-        return 0
+        return a - b
     }
 
     override fun divide(a: Int, b: Int): Int {
         if (b == 0) return 0
-        return 0
+        return a / b
     }
 }
--- src/main/kotlin/com/epam/sup/CalcSuperServiceImpl.kt
+++ src/main/kotlin/com/epam/sup/CalcSuperServiceImpl.kt
@@ -2,10 +2,10 @@ package com.epam.sup
 
 class CalcSuperServiceImpl : CalcSuperService {
     override fun addSuper(a: Int, b: Int, c: Int): Int {
-        return 0
+        return a + b + c
     }
 
     override fun multiplySuper(a: Int, b: Int, c: Int): Int {
-        return 0
+        return a * b * c
     }
 }
